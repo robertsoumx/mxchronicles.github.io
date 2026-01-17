a0bda13f65bc0434be1dd620ca873a19d20d3270
robertsoumx <robertsou2023@outlook.com>
2026-01-17 17:19:51 -0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,8 @@
+const articles = {
+  "1": {title:"History of MX", author:"Alice", date:"2026-01-01", content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.", issue:1, featured:true, image:"https://picsum.photos/300/200?random=1"},
+  "2": {title:"The Old Times", author:"John", date:"2026-01-02", content:"Curabitur sit amet massa sed sapien bibendum feugiat. Duis a justo sit amet leo.", issue:1, featured:false, image:"https://picsum.photos/300/200?random=2"},
+  "3": {title:"Ancient Artifacts", author:"Celine", date:"2026-01-03", content:"Praesent volutpat, justo at tincidunt efficitur, eros libero sagittis odio, eget ultricies.", issue:2, featured:true, image:"https://picsum.photos/300/200?random=3"},
+  "4": {title:"Colonial Stories", author:"Robert", date:"2026-01-04", content:"Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.", issue:2, featured:false, image:"https://picsum.photos/300/200?random=4"}
+};
+
+const staff = ["Alice", "John", "Celine", "Robert"];

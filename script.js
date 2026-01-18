156683f20232a1cc3e3bb08a55d2a4319cb6562c
robertsoumx <robertsou2023@outlook.com>
2026-01-17 19:36:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+let currentIssue = 2;
+
+function setIssue(issueNum) {
+  currentIssue = issueNum;
+  renderHome();
+}
+
 function openArticle(id) {
   window.location.href = `article.html?id=${id}`;
 }
